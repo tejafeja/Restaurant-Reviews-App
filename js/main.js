e38af24e6d3181e8a06b9400219f35284336615b
tejafeja <teja.austeja@gmail.com>
2019-05-08 21:57:07 +0300

    code formatting is correct now; unused code bits removed;

--- js/main.js
+++ js/main.js
@@ -192,14 +192,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 } 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
 
